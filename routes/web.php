59352bea13fed2f3465edb8d5796abdafab7a763
Élie Meignan <contact@eliepse.fr>
2017-10-18 12:46:14 +0200

    Rewrite routes

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+/*
+ * Exemple to fetch all allowed users+streams and refresh all expired
+ * */
+/*$router->get('/streams', function () {
 
 	$start_time = microtime(true);
 
@@ -33,13 +36,51 @@ $router->get('/', function () use ($router) {
 	$data = [];
 
 	foreach ($users as $user) {
-		$data[ $user->login ] = $user->stream()->statut();
+		$data[ $user->login ] = $user->stream()->toArray();
 	}
 
 	return response()->json([
-		'data'            => $data,
-		'updated_streams' => $stream_to_update->pluck('login')->toArray(),
-		'execution_time'  => round(microtime(true) - $start_time, 3),
+		'data'           => $data,
+		'timeout'        => intval(env('APP_REQUEST_TIMEOUT', 60)),
+		'execution_time' => round(microtime(true) - $start_time, 3),
+	]);
+
+});*/
+
+$router->get('/streams/{user_login}/statut', function ($user_login) {
+
+	$start_time = microtime(true);
+
+	$allowed = collect(explode(',', env('TWITCH_USERS')))->filter(function ($item) use ($user_login) {
+		return trim($item) === trim($user_login);
+	});
+
+	if (empty($allowed))
+		return response()->json([
+			'data'           => [],
+			'message'        => [
+				'text' => 'User login not allowed',
+				'type' => 'error',
+			],
+			'execution_time' => round(microtime(true) - $start_time, 3),
+		]);
+
+
+	$api = new \App\TwitchAPI();
+	$user = \App\TwitchUser::find($user_login);
+
+	if ($user->isExpired())
+		$api->fetchUsersData(collect([$user]));
+
+	if ($user->stream()->isExpired())
+		$api->fetchUsersStreamData(collect([$user]));
+
+	return response()->json([
+		'data'           => [
+			$user->getKey() => $user->stream()->isLive,
+		],
+		'timeout'        => intval(env('APP_REQUEST_TIMEOUT', 60)),
+		'execution_time' => round(microtime(true) - $start_time, 3),
 	]);
 
 });
